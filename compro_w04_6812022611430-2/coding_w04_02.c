5dda5582978a304621ddadde1b3dab18cb51a446
Thinanya68012022611430 <tinanya1996@gmail.com>
2025-07-15 22:58:07 +0700

    Add files via upload

---
+++ compro_w04_6812022611430-2/coding_w04_02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  // ขาด # หน้า include
+
+int main() {         
+    int a = 10;      // ไม่มี ; ท้าย int a = 10
+    
+    printf("a = %d\n", a);  
+    
+    return 0;        // ลืมใส่ ; ท้ายคำสั่ง
+}
