56adfbf522cbd8daf23d8b8d064e15143b8ec494
Thinanya68012022611430 <tinanya1996@gmail.com>
2025-07-15 22:57:35 +0700

    Delete compro_w04_6812022611430-2 directory

--- compro_w04_6812022611430-2/coding_w04_02.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>  // ขาด # หน้า include
-
-int main() {         
-    int a = 10;      // ไม่มี ; ท้าย int a = 10
-    
-    printf("a = %d\n", a);  
-    
-    return 0;        // ลืมใส่ ; ท้ายคำสั่ง
-}
