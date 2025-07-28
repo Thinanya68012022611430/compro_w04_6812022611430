1577912b3b882727683ee6b6444eb7213ab37dea
Thinanya68012022611430 <tinanya1996@gmail.com>
2025-07-28 20:49:50 +0700

    Delete compro_w04_6812022611430-2 directory

--- compro_w04_6812022611430-2/coding_w04_01.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>          //    input/output เช่น printf()
-
-int main() {                //    จุดเริ่มต้นของโปรแกรม
-    printf("Hello,\n");     //    แสดงข้อความ Hello
-    printf("C Tokens!\n");  //    แสดงข้อความ C Tokens!
-    return 0;               //    ส่งค่ากลับ 0 เพื่อบอกว่าโปรแกรมทำงานเสร็จสมบูรณ์
-}
